9b2f8ae29aa0f4bf4e32b2b81b2f652f0e9c6c05
user57 <user57@ab791694-f663-0410-bfd7-aac5750c40bb>
2009-10-16 07:54:40 +0000

    Update formatting

--- gmaven-support/filter-plugin/src/main/groovy/org/codehaus/gmaven/filter/Site.groovy
+++ gmaven-support/filter-plugin/src/main/groovy/org/codehaus/gmaven/filter/Site.groovy
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2006-2007 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.codehaus.gmaven.filter;
 
 import java.io.File;
@@ -14,88 +30,87 @@ import org.apache.maven.shared.filtering.MavenResourcesExecution;
 import org.apache.maven.shared.filtering.MavenResourcesFiltering;
 
 /**
- * This simply performs standard Maven filtering on <code>/target/site</code>.  The result is in
- * the same folder, so <code>site:deploy</code> works without a hitch.  This is much simpler to use than
+ * This simply performs standard Maven filtering on <tt>/target/site</tt>.  The result is in
+ * the same folder, so <tt>site:deploy</tt> works without a hitch.  This is much simpler to use than
  * the filtering mechanism built into the site plugin.
+ *
  * @author Jason Smith
  * @requiresDependencyResolution compile
  * @goal site
  * @phase process-resources
  */
-public class Site extends AbstractMojo
+public class Site
+    extends AbstractMojo
 {
-	/**
-	 * The project.
+    /**
+     * The project.
      * @parameter expression="${project}"
      * @required
      * @readonly
      */
     protected MavenProject project;
-    
+
     /**
      * The current session.
      * @parameter expression="${session}"
      * @readonly
      * @required
      */
-    protected MavenSession session;   
-    
+    protected MavenSession session;
+
     /**
      * The filtering object.
      * @component role="org.apache.maven.shared.filtering.MavenResourcesFiltering" role-hint="default"
      * @required
-     */    
-    protected MavenResourcesFiltering mavenResourcesFiltering;    
-    
+     */
+    protected MavenResourcesFiltering mavenResourcesFiltering;
+
     /**
      * The character encoding scheme to be applied when filtering resources.
      * @parameter expression="${encoding}" default-value="${project.build.sourceEncoding}"
      */
     protected String encoding;
-    
+
     /**
      * Expression preceded with the String won't be interpolated 
-     * \${foo} will be replaced with ${foo}
-     * @parameter expression="${maven.resources.escapeString}"
-     */    
+     * \${foo} will be replaced with ${foo}* @parameter expression="${maven.resources.escapeString}"
+     */
     protected String escapeString;
-    
+
     /**
      * Provides filtering for the site.
      */
-	public void execute() throws MojoExecutionException, MojoFailureException 
-	{
-		try
-		{
-			Resource resource = new Resource();
-			resource.setFiltering(true);
-			resource.setDirectory(new File("${project.getBuild().getDirectory()}/site").getCanonicalPath());
-		
-			MavenResourcesExecution mavenResourcesExecution = new MavenResourcesExecution( 
-					 Arrays.asList([resource] as Resource[]), 
-	                 new File("${project.getBuild().getDirectory()}/filtered-site"),
-	                 project, 
-	                 encoding, 
-	                 Collections.EMPTY_LIST,
-	                 Collections.EMPTY_LIST,
-	                 session);
-			 
-			mavenResourcesExecution.setEscapeString(escapeString);
-	        mavenResourcesExecution.setOverwrite(false);
-	        mavenResourcesExecution.setIncludeEmptyDirs(true);
-	         
-	        mavenResourcesFiltering.filterResources(mavenResourcesExecution);
-	        
-	        AntBuilder ant = new AntBuilder();
-	        ant.delete(dir: new File("${project.getBuild().getDirectory()}/site").getCanonicalPath());
-	        ant.copy(todir: new File("${project.getBuild().getDirectory()}/site").getCanonicalPath(), overwrite: true) 
-	        {
-                fileset(dir: new File("${project.getBuild().getDirectory()}/filtered-site").getCanonicalPath())                       
+    public void execute() throws MojoExecutionException, MojoFailureException
+    {
+        try {
+            Resource resource = new Resource();
+            resource.setFiltering(true);
+            resource.setDirectory(new File("${project.getBuild().getDirectory()}/site").getCanonicalPath());
+
+            MavenResourcesExecution mavenResourcesExecution = new MavenResourcesExecution(
+                    Arrays.asList([resource] as Resource[]),
+                    new File("${project.getBuild().getDirectory()}/filtered-site"),
+                    project,
+                    encoding,
+                    Collections.EMPTY_LIST,
+                    Collections.EMPTY_LIST,
+                    session);
+
+            mavenResourcesExecution.setEscapeString(escapeString);
+            mavenResourcesExecution.setOverwrite(false);
+            mavenResourcesExecution.setIncludeEmptyDirs(true);
+
+            mavenResourcesFiltering.filterResources(mavenResourcesExecution);
+
+            AntBuilder ant = new AntBuilder();
+            ant.delete(dir: new File("${project.getBuild().getDirectory()}/site").getCanonicalPath());
+            ant.copy(todir: new File("${project.getBuild().getDirectory()}/site").getCanonicalPath(), overwrite: true)
+            {
+                fileset(dir: new File("${project.getBuild().getDirectory()}/filtered-site").getCanonicalPath())
             }
-		}
-		catch(Exception e)
-		{
-			throw new MojoExecutionException('I\'ve lost my mojo!', e);
-		}
-	}
+        }
+        catch (Exception e) {
+            throw new MojoExecutionException('I\'ve lost my mojo!', e);
+        }
+    }
 }
