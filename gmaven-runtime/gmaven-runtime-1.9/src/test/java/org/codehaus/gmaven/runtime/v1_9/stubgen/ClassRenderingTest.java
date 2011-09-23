18ce0a38316e625261149e271ae9cd2b66d4e276
keeganwitt <keeganwitt@ab791694-f663-0410-bfd7-aac5750c40bb>
2011-09-23 05:24:57 +0000

    initial commit for GMAVEN-90

---
+++ gmaven-runtime/gmaven-runtime-1.9/src/test/java/org/codehaus/gmaven/runtime/v1_9/stubgen/ClassRenderingTest.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) 2007 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.codehaus.gmaven.runtime.v1_9.stubgen;
+
+import org.codehaus.gmaven.runtime.v1_9.RendererTestSupport;
+
+/**
+ * Tests for rendering classes.
+ *
+ * @version $Id: ClassRenderingTest.java 50 2009-10-16 14:06:18Z user57 $
+ * @author <a href="mailto:jason@planet57.com">Jason Dillon</a>
+ */
+public class ClassRenderingTest
+    extends RendererTestSupport
+{
+    public void testRender() throws Exception {
+        chew("classes.groovy.txt");
+    }
+}
