5ce36dbbda99d62c4f3c9b78928eb80a9cf28c74
Jason Dillon <jason@planet57.com>
2012-08-04 19:33:44 -0700

    Wagon: Deploying site to repository

---
+++ groovy-maven-plugin/apidocs/reference/lists.js
@@ -0,0 +1,11 @@
+var DATA = [
+      { id:0, label:"HelpMojo", link:"default package/HelpMojo.html", type:"class" },
+      { id:1, label:"default package", link:"default package/package-summary.html", type:"package" },
+      { id:2, label:"org.codehaus.gmaven.plugin", link:"org/codehaus/gmaven/plugin/package-summary.html", type:"package" },
+      { id:3, label:"org.codehaus.gmaven.plugin.ClassRealms", link:"org/codehaus/gmaven/plugin/ClassRealms.html", type:"class" },
+      { id:4, label:"org.codehaus.gmaven.plugin.MojoSupport", link:"org/codehaus/gmaven/plugin/MojoSupport.html", type:"class" },
+      { id:5, label:"org.codehaus.gmaven.plugin.ResolveMojo", link:"org/codehaus/gmaven/plugin/ResolveMojo.html", type:"class" },
+      { id:6, label:"org.codehaus.gmaven.plugin.TestMojo", link:"org/codehaus/gmaven/plugin/TestMojo.html", type:"class" },
+      { id:7, label:"org.codehaus.gmaven.plugin.VersionDetector", link:"org/codehaus/gmaven/plugin/VersionDetector.html", type:"class" }
+
+    ];
