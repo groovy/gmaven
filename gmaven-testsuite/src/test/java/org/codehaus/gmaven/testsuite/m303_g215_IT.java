e7a3945a23ab6d8d942885c505ae6282f8236538
Jason Dillon <jason@planet57.com>
2013-06-14 18:40:58 -0700

    playing with suites

---
+++ gmaven-testsuite/src/test/java/org/codehaus/gmaven/testsuite/m303_g215_IT.java
@@ -0,0 +1,22 @@
+package org.codehaus.gmaven.testsuite;
+
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+/**
+ * ???
+ */
+@RunWith(Suite.class)
+@SuiteClasses(
+    VerifierXIT.class
+)
+public class m303_g215_IT
+{
+  @BeforeClass
+  public static void configureTestsuite() {
+    System.setProperty("maven.version", "3.0.3");
+    System.setProperty("groovy.version", "2.1.5");
+  }
+}
