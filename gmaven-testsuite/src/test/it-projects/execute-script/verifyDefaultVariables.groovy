0d1d427daffa05bf536413be12cc6c5a66db3483
Jason Dillon <jason@planet57.com>
2013-06-14 17:05:11 -0700

    playing with some options to run integration-tests

---
+++ gmaven-testsuite/src/test/it-projects/execute-script/verifyDefaultVariables.groovy
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2007-2013, the original author or authors.
+ *
+ * This program is licensed to you under the Apache License Version 2.0,
+ * and you may not use this file except in compliance with the Apache License Version 2.0.
+ * You may obtain a copy of the Apache License Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the Apache License Version 2.0 is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
+ */
+
+//noinspection GroovyAssignabilityCheck
+def assertVariable = { value, Class expectedType ->
+  println value
+  println value?.getClass()
+  assert value != null
+  assert expectedType.isAssignableFrom(value.getClass())
+  println 'OK'
+}
+
+assertVariable(project, org.apache.maven.project.MavenProject)
+assertVariable(basedir, java.io.File)
+assertVariable(properties, java.util.Properties)
+assertVariable(ant, groovy.util.AntBuilder)
+assertVariable(fail, groovy.lang.Closure)
+assertVariable(log, org.slf4j.Logger)
+
+assertVariable(container, org.codehaus.gmaven.plugin.util.ContainerHelper)
+assertVariable(plugin, org.apache.maven.plugin.descriptor.PluginDescriptor)
+assertVariable(pluginContext, java.util.Map)
+assertVariable(mojo, org.apache.maven.plugin.MojoExecution)
+assertVariable(session, org.apache.maven.execution.MavenSession)
+assertVariable(settings, org.apache.maven.settings.Settings)
+
+println 'ALL OK'
--- gmaven-testsuite/src/test/java/org/codehaus/gmaven/testsuite/VerifierIT.java
+++ gmaven-testsuite/src/test/java/org/codehaus/gmaven/testsuite/VerifierIT.java
@@ -10,6 +10,7 @@
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
  */
+
 package org.codehaus.gmaven.testsuite;
 
 import java.io.File;
@@ -27,28 +28,30 @@ import org.junit.Test;
 public class VerifierIT
     extends TestSupport
 {
-  @Test
-  public void test() throws Exception {
+  private Verifier createVerifier(final String projectName) throws Exception {
     System.setProperty("verifier.forkMode", "embedded");
-    System.setProperty("maven.home", util.resolvePath("target/filesets/apache-maven-3.0.5"));
+    System.setProperty("maven.home", util.resolvePath("target/filesets/apache-maven-3.0.3"));
 
-    File projectDir = prepareProjectDir("with-classes");
-    File localRepo = util.resolveFile("target/maven-localrepo");
+    File projectDir = prepareProjectDir(projectName);
+    log("Project dir: {}", projectDir);
+
+    File settingsFile = util.resolveFile("target/test-classes/settings.xml");
+    log("Settings file: {}", settingsFile);
 
     Verifier verifier = new Verifier(
         projectDir.getAbsolutePath(),
-        null, // settings
-        true, // debug
-        false // fork
+        settingsFile.getAbsolutePath()
     );
-    verifier.setLocalRepo(localRepo.getAbsolutePath());
+
+    // this can be pretty slow
+    //File localRepo = util.resolveFile("target/maven-localrepo");
+    //log("Local repo: {}", localRepo);
+    //verifier.setLocalRepo(localRepo.getAbsolutePath());
+
+    // some issues with streams not resetting?
     //verifier.resetStreams();
 
-    log("Executing goals");
-    verifier.executeGoal("verify");
-
-    log("Verifying results");
-    verifier.verifyErrorFreeLog();
+    return verifier;
   }
 
   private File prepareProjectDir(final String projectName) throws IOException {
@@ -64,4 +67,21 @@ public class VerifierIT
 
     return targetDir;
   }
+
+
+  @Test
+  public void execute_groovyVersion() throws Exception {
+    Verifier verifier = createVerifier("execute-script");
+
+    String testName = "groovyVersion";
+    verifier.getSystemProperties().setProperty("testName", testName);
+    verifier.getSystemProperties().setProperty("source", "${project.basedir}/${testName}.groovy");
+    verifier.setLogFileName(testName + "-test.log");
+
+    verifier.executeGoal("groovy:execute");
+    verifier.verifyErrorFreeLog();
+    verifier.verifyTextInLog("ALL OK");
+    verifier.verifyTextInLog("Version: 2.1.4");
+  }
+
 }
