3a15b47a976eb1ea86c9acad94d087b05473f066
Jason Dillon <jason@planet57.com>
2013-06-15 23:51:53 -0700

    scriptpath tests

---
+++ gmaven-testsuite/src/test/java/org/codehaus/gmaven/testsuite/tests/Scriptpath.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2007-2013, the original author or authors.
+ *
+ * This program is licensed to you under the Apache License Version 2.0,
+ * and you may not use this file except in compliance with the Apache License Version 2.0.
+ * You may obtain a copy of the Apache License Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the Apache License Version 2.0 is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
+ */
+
+package org.codehaus.gmaven.testsuite.tests;
+
+import org.codehaus.gmaven.testsuite.ITSupport;
+import org.junit.Test;
+
+/**
+ * Verify property resolution.
+ */
+public class Scriptpath
+    extends ITSupport
+{
+  @Test
+  public void scriptpathResolvesClass() throws Exception {
+    verifier("scriptpath")
+        .addProfile(testName.getMethodName())
+        .setProperty("source", "hello.groovy")
+        .executeGoal(goal("execute"))
+        .errorFree()
+        .logContains("Hello World");
+  }
+
+  @Test
+  public void peerResolvesClass() throws Exception {
+    verifier("scriptpath")
+        .addProfile(testName.getMethodName())
+        .setProperty("source", "scripts/goodbye.groovy")
+        .executeGoal(goal("execute"))
+        .errorFree()
+        .logContains("Goodbye World");
+  }
+}
