1076f4d1f6b919cf4ba006d43b5362b11c574ba6
Jason Dillon <jdillon@users.noreply.github.com>
2018-07-18 16:44:18 -0700

    Maven-generated site content refresh

--- apidocs/assets/navtree_data.js
+++
@@ -1,9 +0,0 @@
-var NAVTREE_DATA =
-[ [ "default package", "default package/package-summary.html", [ [ "Classes", null, [ [ "HelpMojo", "default package/HelpMojo.html", null, "" ] ]
-, "" ] ]
-, "" ], [ "org.codehaus.gmaven.plugin", "org/codehaus/gmaven/plugin/package-summary.html", [ [ "Classes", null, [ [ "ClassRealms", "org/codehaus/gmaven/plugin/ClassRealms.html", null, "" ], [ "MojoSupport", "org/codehaus/gmaven/plugin/MojoSupport.html", null, "" ], [ "ResolveMojo", "org/codehaus/gmaven/plugin/ResolveMojo.html", null, "" ], [ "TestMojo", "org/codehaus/gmaven/plugin/TestMojo.html", null, "" ], [ "VersionDetector", "org/codehaus/gmaven/plugin/VersionDetector.html", null, "" ] ]
-, "" ] ]
-, "" ] ]
-
-;
-
